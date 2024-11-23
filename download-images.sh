81af6b042df9a82c89650cfbfa6b08fdeb9645bd
Lex Christopherson <lexchristopherson@Lexs-MacBook-Pro.local>
2024-11-22 21:35:32 -0600

    Add placeholder artist images

---
+++ download-images.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Download placeholder images for artists
+curl "https://picsum.photos/800/800" -o "public/artists/taches.jpg"
+curl "https://picsum.photos/800/800" -o "public/artists/jackson-englund.jpg"
+curl "https://picsum.photos/800/800" -o "public/artists/evan-hatfield.jpg"
